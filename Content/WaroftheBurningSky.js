8753d50ec17b2ee7c7d58e6e9974dba873fc375a
CountVladmir <andrew.grabauskas@googlemail.com>
2021-03-14 23:28:39 +0000

    Create WaroftheBurningSky.js

---
+++ Content/WaroftheBurningSky.js
@@ -0,0 +1,12 @@
+var iFileName = "War of the Burning Sky";
+RequiredSheetVersion(13);
+
+// Define the source
+SourceList["UA:FF"] = {
+	name : "EN Publishing: War of the Burning Sky",
+	abbreviation : "ENP:WBS",
+	group : "DriveThruRPG",
+	url : "https://www.drivethrurpg.com/product/242812/War-of-the-Burning-Sky-5E-Players-Guide",
+	date : "2018/05/24"
+};
+
