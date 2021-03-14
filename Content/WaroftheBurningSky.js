a6f0fb5383daa1fea155ed82ab30dab3b5726c0a
CountVladmir <andrew.grabauskas@googlemail.com>
2021-03-14 23:53:06 +0000

    Update WaroftheBurningSky.js

--- Content/WaroftheBurningSky.js
+++ Content/WaroftheBurningSky.js
@@ -2,7 +2,7 @@ var iFileName = "War of the Burning Sky";
 RequiredSheetVersion(13);
 
 // Define the source
-SourceList["UA:FF"] = {
+SourceList["ENP:WBS"] = {
 	name : "EN Publishing: War of the Burning Sky",
 	abbreviation : "ENP:WBS",
 	group : "DriveThruRPG",
@@ -10,3 +10,16 @@ SourceList["UA:FF"] = {
 	date : "2018/05/24"
 };
 
+SpellsList["cancel"] = {
+	name : "Cancel",
+	classes : ["bard", "druid", "paladin", "ranger", "sorcerer", "warlock", "wizard"],
+	source : ["ENP:WBS", 36],
+	level : 2,
+	school : "Abjur",
+	time : "1 a",
+	range : "Self",
+	components : "V,S",
+	duration : "Conc, 1 min",
+	description : "Form sphere of antimagic, can throw to end concentration or casting of a spell",
+	descriptionFull : "I form a sphere of antimagic in my hand. As a bonus action, I can throw it at a creature I think is concentrating on a spell, as a reaction I can throw it at a creature I see casting a spell. For each spell they are casting or concentrating on of this spell's level or lower en, for higher I must make a spellcasting ability check (DC of 10+spell's level) to end it."
+};
