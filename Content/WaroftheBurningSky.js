e232ad4b102e23a07ce564d09cd5d69b99215f26
CountVladmir <andrew.grabauskas@googlemail.com>
2021-03-15 00:04:07 +0000

    Update WaroftheBurningSky.js

--- Content/WaroftheBurningSky.js
+++ Content/WaroftheBurningSky.js
@@ -23,3 +23,18 @@ SpellsList["cancel"] = {
 	description : "Form sphere of antimagic, can throw to end concentration or casting of a spell",
 	descriptionFull : "I form a sphere of antimagic in my hand. As a bonus action, I can throw it at a creature I think is concentrating on a spell, as a reaction I can throw it at a creature I see casting a spell. For each spell they are casting or concentrating on of this spell's level or lower en, for higher I must make a spellcasting ability check (DC of 10+spell's level) to end it."
 };
+
+SpellsList["duelist's etiquette"] = {
+	name : "Duelist's Etiquette",
+	classes : ["cleric", "sorcerer", "wizard"],
+	source : ["ENP:WBS", 37],
+	level : 2,
+	school : "abjur",
+	time : "1 min",
+	range : "Touch",
+	components : "V,S,M (a pair of padded sticks)",
+	duration : "1 hour",
+	description : "30ft radius where all spell damage becomes non-lethal",
+	descriptionFull : "I touch the ground, and a defensive barrier springs up with a 30ft radius. All damage from spells and summoned creatures originating in the area becomes non-lethal. A creature entering the area must conciously choose to accept the effect, or the spell ends, and any creature inside the area can use an action to end the spell.."
+	ritual : true
+};
